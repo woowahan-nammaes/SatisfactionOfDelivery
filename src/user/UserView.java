504fbc2fb4d77c92af925732e3a8fe44a8cd7fe6
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-07-12 17:42:23 +0900

    feature : UserView에 printSignupTitl() 메소드 추가
    회원가입 화면 타이틀 UI를 위함

--- src/user/UserView.java
+++ src/user/UserView.java
@@ -2,3 +2,9 @@ package user;
 
 public class UserView {
 }
+
+    public void printSignupTitle(){
+        System.out.println("=================회원가입=================");
+        System.out.println("회원가입을 진행합니다.\n아이디, 비밀번호, 닉네임, 주소, 전화번호가 필요해요.\n정보를 입력해주세요!");
+        System.out.println("========================================");
+    }
