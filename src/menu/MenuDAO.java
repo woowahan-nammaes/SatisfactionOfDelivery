7da14135be356ebf5825ef7df5338137fb26f6fe
kyeongseo90 <rudtj9099@gmail.com>
2024-07-12 16:31:45 +0900

    feature: menuDAO 에서 db 정의, menu를 save하는 메소드 구현

--- src/menu/MenuDAO.java
+++ src/menu/MenuDAO.java
@@ -1,4 +1,16 @@
 package menu;
 
+import java.util.*;
+
 public class MenuDAO {
+
+    private static long key = 0;
+    Map<Long, Menu> menuDb = new HashMap<>();
+
+    public Menu save(Menu menu) {
+        menu.setId(key);
+        menuDb.put(key, menu);
+        key += 1;
+        return menu;
+    }
 }
