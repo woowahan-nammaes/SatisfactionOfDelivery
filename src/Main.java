18c6e3b71d5f9e682a1dd94a01e5f75b8d199ead
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-12 15:51:38 +0900

    feature: controller 인스턴스 생성
    메인 페이지에서 분기를 위해 UserController, StoreController, OrderController를 생성했습니다.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import order.OrderController;
+import store.StoreController;
+import user.UserController;
 public class Main {
+    private static UserController userController = new UserController();
+    private static StoreController storeController = new StoreController();
+    private static OrderController orderController = new OrderController();
+
     public static void main(String[] args) {
 
     }
