d4092120dc7200b8985f3c4ecfbab066fd8f48fb
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-12 15:54:27 +0900

    feature: User 객체 인스턴스 생성
    - 메인 화면 시작 시, 비어있는 User 인스턴스를 생성합니다.
    - isLogin을 가져옵니다.(false이면 비회원화면, ture이면 회원화면 출력)
    - option 선택을 위해 Scanner 객체를 생성합니다.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import order.OrderController;
 import store.StoreController;
+import user.User;
 import user.UserController;
+
+import java.util.Scanner;
+
 public class Main {
     private static UserController userController = new UserController();
     private static StoreController storeController = new StoreController();
     private static OrderController orderController = new OrderController();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        User user = new User();
+        boolean isLogin = user.getIsLogin();
 
     }
 }
