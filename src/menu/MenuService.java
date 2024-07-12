19606982f77b9ef389b283b96c326868720e19ec
kyeongseo90 <rudtj9099@gmail.com>
2024-07-12 16:27:55 +0900

    feat: menu service에서 store id에 따라 해당 가게의 메뉴들을 조회하는 메소드 작성

--- src/menu/MenuService.java
+++ src/menu/MenuService.java
@@ -1,4 +1,12 @@
 package menu;
 
+import java.util.List;
+
 public class MenuService {
+
+    private static final MenuDAO menuDAO = new MenuDAO();
+
+    public List<Menu> getStoreMenus(long storeId) {
+        return menuDAO.getStoreMenus(storeId);
+    }
 }
