a30bdaca35baec60e258f226d7f8c97a551a19ea
kyeongseo90 <rudtj9099@gmail.com>
2024-07-12 16:29:15 +0900

    feature: controller에서 store의 menu들을 받아오는 메소드 구현
    storeService와 menuView/Service를 모두 호출해야 합니다

--- src/menu/MenuController.java
+++ src/menu/MenuController.java
@@ -1,4 +1,21 @@
 package menu;
 
+import store.StoreService;
+
+import java.util.List;
+
 public class MenuController {
+
+    private static final MenuService menuService = new MenuService();
+    private static final MenuView menuView = new MenuView();
+    private static final StoreService storeService = new StoreService();
+
+    public void getStoreMenus(long storeId){
+
+        List<Menu> menus = menuService.getStoreMenus(storeId);
+
+        String storeName = storeService.getStoreById(storeId).getName();
+        menuView.getMenu(menus, storeName);
+
+    }
 }
