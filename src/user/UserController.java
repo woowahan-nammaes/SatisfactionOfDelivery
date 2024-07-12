7d1e18bcc43cf83abbf2c863818ddabb27c7fee6
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-07-12 17:47:41 +0900

    feature : UserController에 signpUp()메소드 추기
    회원가입을 위함

--- src/user/UserController.java
+++ src/user/UserController.java
@@ -2,3 +2,13 @@ package user;
 
 public class UserController {
 }
+    UserView userView = new UserView();
+    UserService userService = new UserService();
+
+    public void signUp(){
+        userView.printSignupTitle();
+        String[] signupInfos = userView.getSignupForm();
+        userService.signUp(signupInfos[0], signupInfos[1], signupInfos[2], signupInfos[3], signupInfos[4]);
+        userView.printSuccessSignup();
+    }
+
