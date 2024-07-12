18ab02d2b657cbbf85fdb24b09b4bca3e781b388
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-07-12 17:36:15 +0900

    feature : userdb에 User 객체 insert 기능 구현
    회원가입을 위함

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -2,3 +2,12 @@ package user;
 
 public class UserDAO {
 }
+    private static Map<Long, User> userdb = new HashMap<>();
+    private static long id = 0L;
+
+    public User save(User user){
+        user.setId(++id);
+        userdb.put(user.getId(), user);
+
+        return user;
+    }
