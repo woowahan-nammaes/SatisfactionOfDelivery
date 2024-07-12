111178aaaa3cee12341738bdc35530bb31e1be01
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-07-12 17:40:02 +0900

    faeture : UserService에서 signUp메소드 추가
    회원가입을 위함

--- src/user/UserService.java
+++ src/user/UserService.java
@@ -2,3 +2,17 @@ package user;
 
 public class UserService {
 }
+    public UserDAO userDao = new UserDAO();
+
+    public User signUp(String loginId, String password, String name, String address, String phoneNumber){
+        if (userDao.readByLoginId(loginId) != null) {
+            System.out.println("동일한 id가 존재합니다.");
+            return null;
+        }
+
+        User user = new User(loginId,password,name,address,phoneNumber,false);
+
+        System.out.println("회원가입이 되었어요.");
+        return userDao.save(user);
+    }
+
