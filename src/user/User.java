c5cfec50f45fa14b7695888fc79451d87ff4eef8
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-07-12 17:34:26 +0900

    fix : 생성자 파라미터 순서 변경
    생성자 파라미터 순서로 인한 회원가입 오류 발생 해결

--- src/user/User.java
+++ src/user/User.java
@@ -2,12 +2,21 @@ package user;
 
 public class User {
     private Long id;
-    private String name;
     private String loginId;
     private String password;
+    private String name;
     private String address;
     private String phoneNumber;
     private Boolean isLogin;
+
+    public User(String loginId, String password,String name, String address, String phoneNumber, Boolean isLogin) {
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.isLogin = isLogin;
+    }
 
     public Long getId() {
         return id;
