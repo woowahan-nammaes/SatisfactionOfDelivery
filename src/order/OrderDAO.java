fbd2269d4066cfb48914451d914d635fe6e64dd7
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-07-13 20:51:55 +0900

    feature: order 생성 기능 추가

--- src/order/OrderDAO.java
+++ src/order/OrderDAO.java
@@ -1,4 +1,13 @@
 package order;
 
 public class OrderDAO {
+    public List<Order> getOrdersByUserId(Long userId) {
+        List<Order> orders = new ArrayList<Order>();
+        for (Order order : orderDb.values()) {
+            if (Objects.equals(order.getUser().getId(), userId)) {
+                orders.add(order);
+            }
+        }
+        return orders;
+    }
 }
--- src/order/OrderService.java
+++ src/order/OrderService.java
@@ -1,4 +1,8 @@
 package order;
 
 public class OrderService {
+
+    public List<Order> getUserOrders(Long userId) {
+        return orderDAO.getOrdersByUserId(userId);
+    }
 }
