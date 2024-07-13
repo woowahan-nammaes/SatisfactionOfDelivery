b15d146b146b945595f1ef15560ff4ae7ea1edba
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-07-13 20:45:35 +0900

    feature: Oreder 객체 기능 추가

--- src/order/Order.java
+++ src/order/Order.java
@@ -14,5 +14,45 @@ public class Order {
     private OrderStatus status;
     private LocalDateTime createdAt;
 
+    public Order() {
+    }
 
+    public Order(Store store, Menu menu, User user, OrderStatus status) {
+        this.store = store;
+        this.menu = menu;
+        this.user = user;
+        this.status = status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
