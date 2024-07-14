3e095ed71654d93a9fb1c813125018c5b3b6ed76
kyeongseo90 <rudtj9099@gmail.com>
2024-07-15 08:40:43 +0900

    style: Db 변수명 변경

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -4,24 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserDAO {
-    private static Map<Long, User> userdb = new HashMap<>();
+
+    private static final UserDAO userDAO = new UserDAO();
+    private static final Map<Long, User> userDb = new HashMap<>();
     private static long id = 0L;
 
     public User save(User user){
         user.setId(++id);
-        userdb.put(user.getId(), user);
+        userDb.put(user.getId(), user);
 
         return user;
     }
     public User readByLoginId(String loginId){
-        for (User user : userdb.values()){
+        for (User user : userDb.values()){
             if(user.getLoginId().equals(loginId)) return user;
         }
         return null;
     }
     public User login(String loginId, String password){
 
-        for (User user : userdb.values()) {
+        for (User user : userDb.values()) {
             if(user.getLoginId().equals(loginId) && user.getPassword().equals(password)) {
                 user.setIsLogin(true);
                 return user;
@@ -30,9 +32,9 @@ public class UserDAO {
         return null;
     }
     public User logout(String loginId){
-        User user = userdb.get(id);
+        User user = userDb.get(id);
         user.setIsLogin(false);
-        userdb.put(id, user);
+        userDb.put(id, user);
         return user;
     }
 }
